e13b3cdcf4d1995b7f34f4b23a223a24f363d0b5
Sergio <nakul.sv@gmail.com>
2019-05-10 23:26:59 +0300

    Add department, user and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_10_185706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "active"
+    t.bigint "department_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["department_id"], name: "index_employees_on_department_id"
+  end
+
+  add_foreign_key "employees", "departments"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+ACTIVE = ['Yes', 'No']
+
+departments = Department.create!([{ name: 'HR' }, { name: 'Tech' }, { name: 'Finance' }])
+
+ACTIVE.each do |act|
+  departments.each do |department|
+    100.times { Employee.create!(name: Faker::Name.first_name, department_id: department.id, active: act) }
+  end
+end
+
+p "Created #{Department.count} departments"
+p "Created #{Employee.count} users"
